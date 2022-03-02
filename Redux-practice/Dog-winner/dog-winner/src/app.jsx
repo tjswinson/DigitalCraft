d2c4c072fb88f97f5c94f8fc1a12130a0090f6a2
tjswinson <tiffanyjswinson@gmail.com>
2022-03-01 20:30:51 -0500

    rename boilerplate files to jsx

--- Redux-practice/Dog-winner/dog-winner/src/app.jsx
+++ Redux-practice/Dog-winner/dog-winner/src/app.jsx
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 function App() {
+ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +19,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        Hello World
+        </div>
     </div>
-  );
+  )};
 }
 
 export default App;
