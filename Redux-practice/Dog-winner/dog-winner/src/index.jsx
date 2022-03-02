cd6eed25127a5592d27db21744cb9a8d28042f1f
tjswinson <tiffanyjswinson@gmail.com>
2022-03-01 20:48:43 -0500

    remove unnecessary boilerplate code

--- Redux-practice/Dog-winner/dog-winner/src/index.jsx
+++ Redux-practice/Dog-winner/dog-winner/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './style.css'
+import './style.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
 
