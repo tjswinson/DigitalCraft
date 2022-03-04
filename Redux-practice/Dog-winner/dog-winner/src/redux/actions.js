ed6c604efd1efea6481ac25b0f0e98d004a89895
tjswinson <tiffanyjswinson@gmail.com>
2022-03-03 19:53:50 -0500

    randomize array of questions before they are added to Redux store

--- Redux-practice/Dog-winner/dog-winner/src/redux/actions.js
+++ Redux-practice/Dog-winner/dog-winner/src/redux/actions.js
@@ -1,9 +1,10 @@
 import data from ' ./initialState.json';
 import { SET_INITIAL_STATE } from './actionTypes';
+import shuffle from '../utils/shuffle';
 
 export const setInitialState = () => {
     return {
         type: SET_INITIAL_STATE,
-        data
+        data: shuffle(data)
     }
 };
---
+++ Redux-practice/Dog-winner/dog-winner/src/utils/shuffle.js
@@ -0,0 +1,14 @@
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+export default function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [
+        array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]
+      ];
+    }
+  
+    return array;
+  }
